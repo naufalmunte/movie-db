742e94b8b76074e0f25ca4ace329b9a9e829a9e3
Naufal Hilmy Munte <naufalhilmymunte@gmail.com>
2025-05-07 15:21:17 +0700

    make migration

---
+++ database/migrations/2025_05_07_075445_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_name', 128);
+            $table->string('description');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop the categories table when rolling back the migration
+        Schema::dropIfExists('categories');
+    }
+}
